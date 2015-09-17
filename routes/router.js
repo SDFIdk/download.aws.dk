7b73c261c7462c0ce9c916c2ec2fa0d43b9e0f24
Finn Jordal <finn@jordal.dk>
2015-09-17 15:21:04 +0200

    Kopi

---
+++ routes/router.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var request = require('request');
+var async = require('async');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/adresser', function(req, res) {
+	async.parallel([
+		function(callback) {
+			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+  			callback(null,JSON.parse(body));
+			});
+		},
+		function(callback) {
+			request("http://dawa.aws.dk/regioner",function(error, response, body) {
+  			callback(null,JSON.parse(body));
+			});
+		}],
+		function(err, result) {
+			res.render('adresser', { kommuner: result[0], regioner: result[1], url: "http://dawa.aws.dk/adresser" });
+		});
+});
+
+router.get('/adgangsadresser', function(req, res) {
+	async.parallel([
+		function(callback) {
+			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+  			callback(null,JSON.parse(body));
+			});
+		},
+		function(callback) {
+			request("http://dawa.aws.dk/regioner",function(error, response, body) {
+  			callback(null,JSON.parse(body));
+			});
+		}],
+		function(err, result) {
+			res.render('adgangsadresser', { kommuner: result[0], regioner: result[1], url: "http://dawa.aws.dk/adgangsadresser" });
+		});
+});
+
+router.get('/vejnavne', function(req, res) {
+	async.parallel([
+		function(callback) {
+			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+  			callback(null,JSON.parse(body));
+			});
+		}],
+		function(err, result) {
+			res.render('vejnavne', { kommuner: result[0], url: "http://dawa.aws.dk/vejstykker" });
+		});
+});
+
+router.get('/supplerendebynavne', function(req, res) {
+	async.parallel([
+		function(callback) {
+			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+  			callback(null,JSON.parse(body));
+			});
+		}],
+		function(err, result) {
+			res.render('supplerendebynavne', { kommuner: result[0], url: "http://dawa.aws.dk/supplerendebynavne" });
+		});
+});
+
+router.get('/postnumre', function(req, res) {
+	res.render('postnumre', { url: "http://dawa.aws.dk/postnumre" });
+});
+
+router.get('/om', function(req, res) {
+	res.render('om');
+});
+
+module.exports = router;
