920265b168e3ec48f13a96603ef5d79890544071
Signe <signees96@gmail.com>
2021-03-11 14:31:09 +0100

    Fixed subdomain, domain and protocols in router.js

--- routes/router.js
+++ routes/router.js
@@ -15,63 +15,63 @@ router.get('/licens', function(req, res) {
 router.get('/adresser', function(req, res) {
 	async.parallel([
 		function(callback) {
-			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+			request("https://api.dataforsyningen.dk/kommuner",function(error, response, body) {
   			callback(null,JSON.parse(body));
 			});
 		},
 		function(callback) {
-			request("http://dawa.aws.dk/regioner",function(error, response, body) {
+			request("https://api.dataforsyningen.dk/regioner",function(error, response, body) {
   			callback(null,JSON.parse(body));
 			});
 		}],
 		function(err, result) {
-			res.render('adresser', { kommuner: result[0], regioner: result[1], url: "http://dawa.aws.dk/adresser" });
+			res.render('adresser', { kommuner: result[0], regioner: result[1], url: "https://api.dataforsyningen.dk/adresser" });
 		});
 });
 
 router.get('/adgangsadresser', function(req, res) {
 	async.parallel([
 		function(callback) {
-			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+			request("https://api.dataforsyningen.dk/kommuner",function(error, response, body) {
   			callback(null,JSON.parse(body));
 			});
 		},
 		function(callback) {
-			request("http://dawa.aws.dk/regioner",function(error, response, body) {
+			request("https://api.dataforsyningen.dk/regioner",function(error, response, body) {
   			callback(null,JSON.parse(body));
 			});
 		}],
 		function(err, result) {
-			res.render('adgangsadresser', { kommuner: result[0], regioner: result[1], url: "http://dawa.aws.dk/adgangsadresser" });
+			res.render('adgangsadresser', { kommuner: result[0], regioner: result[1], url: "https://api.dataforsyningen.dk/adgangsadresser" });
 		});
 });
 
 router.get('/vejnavne', function(req, res) {
 	async.parallel([
 		function(callback) {
-			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+			request("https://api.dataforsyningen.dk/kommuner",function(error, response, body) {
   			callback(null,JSON.parse(body));
 			});
 		}],
 		function(err, result) {
-			res.render('vejnavne', { kommuner: result[0], url: "http://dawa.aws.dk/vejstykker" });
+			res.render('vejnavne', { kommuner: result[0], url: "https://api.dataforsyningen.dk/vejstykker" });
 		});
 });
 
 router.get('/supplerendebynavne', function(req, res) {
 	async.parallel([
 		function(callback) {
-			request("http://dawa.aws.dk/kommuner",function(error, response, body) {
+			request("https://api.dataforsyningen.dk/kommuner",function(error, response, body) {
   			callback(null,JSON.parse(body));
 			});
 		}],
 		function(err, result) {
-			res.render('supplerendebynavne', { kommuner: result[0], url: "http://dawa.aws.dk/supplerendebynavne" });
+			res.render('supplerendebynavne', { kommuner: result[0], url: "https://api.dataforsyningen.dk/supplerendebynavne" });
 		});
 });
 
 router.get('/postnumre', function(req, res) {
-	res.render('postnumre', { url: "http://dawa.aws.dk/postnumre" });
+	res.render('postnumre', { url: "https://api.dataforsyningen.dk/postnumre" });
 });
 
 router.get('/om', function(req, res) {
